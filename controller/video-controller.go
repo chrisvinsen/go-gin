16d1554d898eb8a63241f6ef4849f0b270d0abe4
sen <macbook@macbooks-MacBook-Pro.local>
2021-05-29 13:43:42 +0700

    init

---
+++ controller/video-controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"github.com/chrisvinsen/go-gin/entity"
+	"github.com/chrisvinsen/go-gin/service"
+	"github.com/gin-gonic/gin"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) entity.Video
+}
+
+// implement that interface
+type controller struct {
+	service service.VideoService
+}
+
+func New(service service.VideoService) VideoController {
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return service.New().FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	ctx.BindJSON(&video)
+	c.service.Save(video)
+	return video
+}
